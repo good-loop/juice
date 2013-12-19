6f58589a55e6413bb641c721c47b5b15616e211b
Daniel Winterstein <daniel.winterstein@gmail.com>
2013-12-19 17:51:02 +0000

    bugfix2

--- src/com/winterwell/juice/juicers/CyclingCCJuicer.java
+++ src/com/winterwell/juice/juicers/CyclingCCJuicer.java
@@ -47,70 +47,76 @@ public class CyclingCCJuicer extends AJuicer {
 	private List<Item> juiceIndex(Elements lis, JuiceMe doc) {
 		List<Item> items = new ArrayList(lis.size());
 		for (Element li : lis) {
-			// Text
-			String threadId = null; // <li class="forum i156301">
-			Set<String> classes = li.classNames();
-			for (String c : classes) {
-				if (c.startsWith("i")) {
-					threadId = c;
-					break;
-				}
-			}
-			Elements hs = null;
-			for(int i=2; i<5; i++) {
-				hs = li.getElementsByTag("h"+i);
-				if ( ! hs.isEmpty()) break;
-			}
-			String title = null;
-			if (hs!=null && ! hs.isEmpty()) {
-				title = hs.text(); // In a thread page, we don't get a title
-			}
-			Elements topics = li.getElementsByClass("topic");			
-			String text = topics.text();
-			if (Utils.isBlank(title) && Utils.isBlank(text)) {
-				Log.d("juicer.cc", "skip blank "+li.html());
-				continue;
-			}
-			Item item = new Item(li, doc.getURL());
-			// cycling.cc urls point to pages of items
-			item.stable1ItemUrl = false;
-			if ( ! Utils.isBlank(threadId)) {
-				item.put(anno(THREAD_XID, new XId(threadId+"@"+doc.getDomain(), SiteSpider.SERVICE_WEB), li));
-			}			
-			if ( ! Utils.isBlank(title)) item.put(anno(TITLE, title, hs.get(0)));
-			if ( ! topics.isEmpty()) item.put(anno(DESC, text, topics.get(0)));
-			Elements as = hs!=null && ! hs.isEmpty()? hs.get(0).getElementsByTag("a") : null;
-			if (as!=null && ! as.isEmpty()) {
-				String href = as.attr("href");
-				item.put(anno(URL, href, as.get(0)));
-			}
-			// XID -- will have to be semantic :(
-			// Author
-			as = li.getElementsByTag("a");
-			for (Element a : as) {
-				String href = a.attr("href");
-				if (href.contains("/profile")) {
-					String authName = a.text();
-					if ( ! authName.isEmpty()) {
-						item.put(anno(AUTHOR_NAME, authName, a));
-						item.put(anno(AUTHOR_URL, href, a));
-						item.put(anno(AUTHOR_XID, authName.toLowerCase()+"@"+doc.getDomain(), a));
-						break;
-					}
-				}
-			}
-			Elements img = li.getElementsByTag("img");
-			if ( ! img.isEmpty()) {
-				item.put(anno(AUTHOR_IMG, img.attr("src"), img.get(0)));
-			}
-//			if (item.get(AJuicer.AUTHOR_XID)==null) {
-//				Log.d("juicer.cc", "No oxid? "+li.html());
-//			}
+			Item item = juiceIndex2(li, doc);
+			if (item==null) continue;
 			// DOne
 			doc.addItem(item);
 			items.add(item);
 		}
 		return items;
+	}
+
+	private Item juiceIndex2(Element li, JuiceMe doc) {
+		// Text
+		String threadId = null; // <li class="forum i156301">
+		Set<String> classes = li.classNames();
+		for (String c : classes) {
+			if (c.startsWith("i")) {
+				threadId = c;
+				break;
+			}
+		}
+		Elements hs = null;
+		for(int i=2; i<5; i++) {
+			hs = li.getElementsByTag("h"+i);
+			if ( ! hs.isEmpty()) break;
+		}
+		String title = null;
+		if (hs!=null && ! hs.isEmpty()) {
+			title = hs.text(); // In a thread page, we don't get a title
+		}
+		Elements topics = li.getElementsByClass("topic");			
+		String text = topics.text();
+		if (Utils.isBlank(title) && Utils.isBlank(text)) {
+			Log.d("juicer.cc", "skip blank "+li.html());
+			return null;
+		}
+		Item item = new Item(li, doc.getURL());
+		// cycling.cc urls point to pages of items
+		item.stable1ItemUrl = false;
+		if ( ! Utils.isBlank(threadId)) {
+			item.put(anno(THREAD_XID, new XId(threadId+"@"+doc.getDomain(), SiteSpider.SERVICE_WEB), li));
+		}			
+		if ( ! Utils.isBlank(title)) item.put(anno(TITLE, title, hs.get(0)));
+		if ( ! topics.isEmpty()) item.put(anno(DESC, text, topics.get(0)));
+		Elements as = hs!=null && ! hs.isEmpty()? hs.get(0).getElementsByTag("a") : null;
+		if (as!=null && ! as.isEmpty()) {
+			String href = as.attr("href");
+			item.put(anno(URL, href, as.get(0)));
+		}
+		// XID -- will have to be semantic :(
+		// Author
+		as = li.getElementsByTag("a");
+		for (Element a : as) {
+			String href = a.attr("href");
+			if (href.contains("/profile")) {
+				String authName = a.text();
+				if ( ! authName.isEmpty()) {
+					item.put(anno(AUTHOR_NAME, authName, a));
+					item.put(anno(AUTHOR_URL, href, a));
+					item.put(anno(AUTHOR_XID, authName.toLowerCase()+"@"+doc.getDomain(), a));
+					break;
+				}
+			}
+		}
+		Elements img = li.getElementsByTag("img");
+		if ( ! img.isEmpty()) {
+			item.put(anno(AUTHOR_IMG, img.attr("src"), img.get(0)));
+		}
+//		if (item.get(AJuicer.AUTHOR_XID)==null) {
+//			Log.d("juicer.cc", "No oxid? "+li.html());
+//		}
+		return item;
 	}
 
 }
