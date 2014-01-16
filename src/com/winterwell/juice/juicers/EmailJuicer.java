cc1e796739cd3e7e39d857052c35adde9ddd2107
Daniel <daniel@winterwell.com>
2014-01-16 17:34:10 +0000

    sync

---
+++ src/com/winterwell/juice/juicers/EmailJuicer.java
@@ -0,0 +1,10 @@
+package com.winterwell.juice.juicers;
+
+/**
+ * TODO extract useful bits of info from an email. May be needed for Which?
+ * @author daniel
+ *
+ */
+public class EmailJuicer {
+
+}
---
+++ src/com/winterwell/juice/juicers/PlainTextJuicer.java
@@ -0,0 +1,11 @@
+package com.winterwell.juice.juicers;
+
+
+/**
+ * TODO extract useful bits of info from misc plain text.
+ * @author daniel
+ *
+ */
+public class PlainTextJuicer {
+
+}
