2a33c9b096322fe1cf3c4b36fef222518958a85f
Daniel Winterstein <daniel@sodash.com>
2015-11-16 14:12:52 +0000

    quieter FB no sender messages

--- test/com/winterwell/juice/juicers/LinkedInJuicerTest.java
+++ test/com/winterwell/juice/juicers/LinkedInJuicerTest.java
@@ -16,6 +16,30 @@ public class LinkedInJuicerTest {
 
 //	https://www.linkedin.com/grp/post/5042022-6020180410548383748
 	
+	
+	@Test
+	public void testLIProfileFail() {		
+		String url = "https://www.linkedin.com/in/joshua-luksberg-52867230";
+		File local = TestUtils.getTestFile("linkedin", url);
+		MetaDataJuicer mdj = new MetaDataJuicer();
+		LinkedInJuicer pj = new LinkedInJuicer();
+		String html = FileUtils.read(local);
+		JuiceMe doc = new JuiceMe(url, html);
+		
+		mdj.juice(doc);
+		boolean ok = pj.juice(doc);
+		
+		Item item = doc.getMainItem();
+		System.out.println(item.getTitle());
+		System.out.println(item.getXId());
+		System.out.println(item.getText());
+//		System.out.println(item.getHTML());	
+		System.out.println(item.get(AJuicer.IMAGE_URL));
+		System.out.println(item.get(AJuicer.LINK));
+		System.out.println(item.get(AJuicer.AUTHOR_NAME));
+		assert item.get(AJuicer.AUTHOR_NAME).contains("Joshua") : item;
+	}
+	
 	@Test
 	public void testJuice() {
 		String url = "https://www.linkedin.com/grp/post/7445683-5898412079244681220";
