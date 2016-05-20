4a7c753a6f7c12d9dfabbb4f3480a27722bf3eb5
Daniel Winterstein <daniel@sodash.com>
2016-05-20 15:46:53 +0100

    wip

--- src/com/winterwell/juice/juicers/RSSJuicer.java
+++ src/com/winterwell/juice/juicers/RSSJuicer.java
@@ -7,9 +7,9 @@ import org.jsoup.select.Elements;
 
 import winterwell.utils.Utils;
 import winterwell.utils.time.Time;
-import winterwell.utils.web.WebUtils;
-import winterwell.utils.web.WebUtils2;
-import winterwell.web.fields.DateField;
+import com.winterwell.utils.web.WebUtils;
+import com.winterwell.utils.web.WebUtils2;
+import com.winterwell.web.fields.DateField;
 
 import com.winterwell.juice.AJuicer;
 import com.winterwell.juice.Item;
