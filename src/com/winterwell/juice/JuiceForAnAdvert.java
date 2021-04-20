1ea11012e99a662e4f65a781f121993557129f80
Daniel Winterstein <daniel@good-loop.com>
2021-04-20 17:08:26 +0100

    sync

---
+++ src/com/winterwell/juice/JuiceForAnAdvert.java
@@ -0,0 +1,26 @@
+package com.winterwell.juice;
+
+/**
+ * TODO patch the gaps left by other juicers for:
+ * 
+ * Name
+ * Tagline / slogan
+ * Logo
+ * Top photos
+ * Brand colours
+ * Fonts
+ * CTA link
+ * CTA "action words"
+ * 
+ * @author daniel
+ *
+ */
+public class JuiceForAnAdvert extends AJuicer {
+
+	@Override
+	protected boolean juice(JuiceMe doc) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
