b68816df94bb228a39191590b06b2f7dac72e639
Daniel Winterstein <daniel@good-loop.com>
2021-05-17 17:48:27 +0100

    juice api

--- src/com/winterwell/juice/JuiceConfig.java
+++ src/com/winterwell/juice/JuiceConfig.java
@@ -3,7 +3,7 @@ package com.winterwell.juice;
 import com.winterwell.web.app.BasicSiteConfig;
 
 /**
- * STATUS: not used TODO
+
  * @author daniel
  *
  */
@@ -13,6 +13,6 @@ public class JuiceConfig extends BasicSiteConfig {
 		port = 8343;
 	}
 	
-	public static String version = "0.2.0";
+	public static String version = "0.3.0";
 	
 }
